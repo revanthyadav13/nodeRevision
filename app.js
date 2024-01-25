f6ef7e5fbaac7bca38e4d42d21448c0725a316a2
revanthyadav13 <revanthyadav13@gmail.com>
2024-01-25 12:18:23 +0530

    Routing requets

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    
-    if(req.url=='/home'){
+    let url=req.url
+    if(url==='/'){
         res.setHeader('Content-Type','text/html');
         res.write('<html>');
-        res.write('<head><title>Home Page</title></head>');
-        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><label for="message">Enter Message</label><br><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        res.end();
-    }else if(req.url=='/about'){
+      return  res.end();
+    }
         res.setHeader('Content-Type','text/html');
         res.write('<html>');
         res.write('<head><title>About Us Page</title></head>');
-        res.write('<body><h1> Welcome to About Us page</h1></body>');
+        res.write('<body><h1>Hello form my node.js server!</h1></body>');
         res.write('</html>');
         res.end();
-    }else if(req.url=='/node'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>Node Js project Page</title></head>');
-        res.write('<body><h1> Welcome to my Node Js project</h1></body>');
-        res.write('</html>');
-        res.end(); 
-    }
 
 });
 
