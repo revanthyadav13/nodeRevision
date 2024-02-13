5a23d95aa08ca728e052f79712de7e506ca9fff6
revanthyadav13 <revanthyadav13@gmail.com>
2024-02-13 12:14:39 +0530

    bodyParser

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const http=require('http');
-
 const express = require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req, res, next)=>{
-    console.log('In the middleware!');
-    next();
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
-app.use((req, res, next)=>{
-    console.log('In another middleware!');
+
+app.use('/product', (req, res, next)=>{
+console.log(req.body.title);
+    res.redirect('/')
+})
+app.use('/',(req, res, next)=>{
+    res.send('<h1>Welcome to express</h1>')
 });
 
-const server=http.createServer(app);
-
-
-server.listen(4000);
+app.listen(3000);
